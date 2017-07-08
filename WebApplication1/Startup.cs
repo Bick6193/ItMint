97ed6dbf611b5ae87e2c23f369b820c4a7c077b4
1 <1>
2017-07-08 20:37:04 +0300

    basic setup

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -42,18 +42,14 @@ namespace WebApplication1
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
+            
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Default}/{action=Index}/{id?}");
             });
         }
     }
