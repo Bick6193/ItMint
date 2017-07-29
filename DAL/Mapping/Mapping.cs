06bd36733590fd7b1d56d268476b92df9ac55420
Vlad Petrov <vlad.petrov.phd@gmail.com>
2017-07-29 21:44:47 +0300

    1

---
+++ DAL/Mapping/Mapping.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using AutoMapper;
+
+namespace DAL.Mapping
+{
+    public static class Mapping
+    {
+        public static void Init()
+        {
+            Mapper.Initialize(configuration =>
+            {
+                //configuration.ShouldMapField = ShouldMap;
+                //configuration.ShouldMapProperty = ShouldMap;
+
+                //configuration.AddProfile<UserProfile>();
+            });
+
+            //precompile
+            Mapper.Configuration.CompileMappings();
+        }
+
+        public static bool ShouldMap(FieldInfo field)
+        {
+            return field.IsPublic || field.IsAssembly;
+        }
+
+        public static bool ShouldMap(PropertyInfo property)
+        {
+            return property.GetMethod.IsPublic || property.GetMethod.IsAssembly;
+        }
+    }
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,8 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 using Common;
 using Common.Configuration;
 using Configuration.IoC;
+using Configuration.Mapping;
+using CreditorGuard.Common;
 using DAL.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -19,63 +23,101 @@ namespace WebApplication1
 {
     public class Startup
     {
-        //todo remuve after loggin setup
-        public IConfigurationRoot Configuration { get; }
-
         public ICustomConfigurationProvider ConfigurationProvider { get; }
 
         public Startup(IHostingEnvironment env)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            try
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(env.ContentRootPath)
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                    .AddEnvironmentVariables();
+                var configuration = builder.Build();
 
-            ConfigurationProvider = new CustomConfigurationProvider(Configuration, env.ContentRootPath);
+                ConfigurationProvider = new CustomConfigurationProvider(configuration, env.ContentRootPath);
 
-            LoggerConfig.Configure(ConfigurationProvider);
+                LoggerConfig.Configure(ConfigurationProvider);
+            }
+            catch (Exception e)
+            {
+                Log.Logger().Error(e, "Application failed to start");
+                throw;
+            }
         }
         
         public void ConfigureServices(IServiceCollection services)
         {
-            DependencyMapper.Configure(services, ConfigurationProvider);
+            try
+            {
+                Log.Logger().Information("Configure Dependency Injection...");
 
-            services.AddEntityFramework().AddDbContext<ApplicationContext>(options => options.UseSqlServer(""));
+                DependencyMapper.Configure(services, ConfigurationProvider);
+
+                services.AddEntityFramework().AddDbContext<ApplicationContext>(options => options.UseSqlServer(""));
             
-            services.AddMvc();
+                services.AddMvc();
+            }
+            catch (Exception e)
+            {
+                Log.Logger().Error(e, "Application failed to start");
+                throw;
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddSerilog();
-
-            Log.Logger().Information("Application is starting...");
-
-            Log.Logger().Information("Configuration:");
-
-            ConfigurationProvider.GetType().GetProperties().ToList().ForEach(prop => { Log.Logger().Information("[{name}] = '{value}'", prop.Name, prop.GetValue(ConfigurationProvider)); });
-
-            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            //loggerFactory.AddDebug();
-
-            //todo find out ????
-            if (env.IsDevelopment())
+            var stopwatch = Stopwatch.StartNew();
+            try
             {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
+                loggerFactory.AddSerilog();
+
+                Log.Logger().Information("Application is starting...");
+
+                Log.Logger().Information("Configuration:");
+
+                ConfigurationProvider.GetType().GetProperties().ToList().ForEach(prop =>
+                {
+                    Log.Logger().Information("[{name}] = '{value}'", prop.Name,
+                        prop.GetValue(ConfigurationProvider));
+                });
+
+                DateTimeContext.Initialize(ConfigurationProvider.TimeZone);
+
+                Log.Logger().Information("Configure EF Mappings...");
+
+                MappingConfig.RegisterMappings();
+                
+                //todo find out ????
+                if (env.IsDevelopment())
+                {
+                   // app.UseDeveloperExceptionPage();
+                   // app.UseBrowserLink();
+                }
+
+                app.UseStaticFiles();
+
+                app.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Default}/{action=Index}/{id?}");
+                });
+
             }
-            
-            app.UseStaticFiles();
-
-            app.UseMvc(routes =>
+            catch (Exception e)
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Default}/{action=Index}/{id?}");
-            });
+                Log.Logger().Error(e, "Application failed to start");
+                throw;
+            }
+            finally
+            {
+                stopwatch.Stop();
+
+                Log.Logger().Information("Startup time: {Seconds}s", stopwatch.Elapsed.Seconds);
+            }
         }
     }
 }
