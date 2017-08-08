dd3f1f6e78367ef56a4dfb9552f220c7e6c42900
Vlad Petrov <vlad.petrov.phd@gmail.com>
2017-08-08 21:31:39 +0300

    GenerateToken

---
+++ WebApplication1/Identity/TokenProviderMiddleware.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+
+namespace Web.Identity
+{
+  public class TokenProviderMiddleware
+  {
+    private readonly RequestDelegate _next;
+    private readonly TokenProviderOptions _options;
+    //todo inject users service
+    
+    public TokenProviderMiddleware(RequestDelegate next,
+                                  IOptions<TokenProviderOptions> options)
+    {
+      _next = next;
+      _options = options.Value;
+    }
+    public Task Invoke(HttpContext context)
+    {
+      if (!context.Request.Path.Equals(_options.Path, StringComparison.Ordinal))
+      {
+        return _next(context);
+      }
+      if (!context.Request.Method.Equals("POST") || !context.Request.HasFormContentType)
+      {
+        context.Response.StatusCode = 400;
+        return context.Response.WriteAsync("Bad Request");
+      }
+      return GenerateToken(context);
+    }
+
+    private async Task GenerateToken(HttpContext context)
+    {
+      var username = context.Request.Form["username"];
+      var password = context.Request.Form["password"];
+
+      var identity = await GetIdentity(username, password);
+      if (identity == null)
+      {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync("Invalid username or password");
+        return;
+      }
+      var now = DateTime.UtcNow;
+
+
+      /*var claims = new Claim[]
+      {
+          new Claim(JwtRegisteredClaimNames.Sub, username),
+          new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+          new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
+      };
+      */
+     // var userClaims = await _userManager.GetRolesAsync(user);
+      List<Claim> claims = new List<Claim>();
+      claims.Add(new Claim(JwtRegisteredClaimNames.Sub, username));
+      claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+      claims.Add(new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64));
+      //claims.AddRange(user.Claims.ToArray());
+
+      //foreach (var x in userClaims)
+      //{
+      //  claims.Add(new Claim(ClaimTypes.Role, x));
+      //}
+
+
+      var jwt = new JwtSecurityToken(
+        issuer: _options.Issuer,
+        audience: _options.Audience,
+        claims: claims,
+        notBefore: now,
+        expires: now.Add(_options.Expiration),
+        signingCredentials: _options.SigningCredentials);
+
+      var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+      var response = new
+      {
+        access_token = encodedJwt,
+        expires_in = (int)_options.Expiration.TotalSeconds
+      };
+
+      context.Response.ContentType = "application/json";
+      await context.Response.WriteAsync(JsonConvert.SerializeObject(response, new JsonSerializerSettings { Formatting = Formatting.Indented }));
+    }
+
+    private Task<ClaimsIdentity> GetIdentity(string username, string password)
+    {
+      // DON'T do this in production, obviously!
+      if (username == "TEST" && password == "TEST")
+      {
+        return Task.FromResult(new ClaimsIdentity(new System.Security.Principal.GenericIdentity(username, "Token"), new Claim[] { }));
+      }
+
+      // Credentials are invalid, or account doesn't exist
+      return Task.FromResult<ClaimsIdentity>(null);
+    }
+
+  }
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using Common;
 using Common.Configuration;
 using Configuration.IoC;
@@ -16,8 +17,11 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using Web.Filters;
+using Web.Identity;
 using Log = Common.Log;
 
 
@@ -25,7 +29,11 @@ namespace WebApplication1
 {
     public class Startup
     {
-        public ICustomConfigurationProvider ConfigurationProvider { get; }
+      private static readonly string secretKey = "mysupersecret_secretkey!123";
+      private static readonly string issure = "AltairCA";
+      private static readonly string audience = "AltairCAAudience";
+
+    public ICustomConfigurationProvider ConfigurationProvider { get; }
 
         public Startup(IHostingEnvironment env)
         {
@@ -71,74 +79,123 @@ namespace WebApplication1
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+      public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+      {
+        var stopwatch = Stopwatch.StartNew();
+        try
         {
-            var stopwatch = Stopwatch.StartNew();
-            try
+          loggerFactory.AddSerilog();
+
+          Log.Logger().Information("Application is starting...");
+
+          Log.Logger().Information("Configuration:");
+
+          ConfigurationProvider.GetType().GetProperties().ToList().ForEach(prop =>
+          {
+            Log.Logger().Information("[{name}] = '{value}'", prop.Name,
+              prop.GetValue(ConfigurationProvider));
+          });
+
+          DateTimeContext.Initialize(ConfigurationProvider.TimeZone);
+
+          Log.Logger().Information("Configure EF Mappings...");
+
+          MappingConfig.RegisterMappings();
+
+          //todo find out ????
+          if (env.IsDevelopment())
+          {
+            // app.UseDeveloperExceptionPage();
+            // app.UseBrowserLink();
+          }
+
+          //start jwt token config
+          var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+          //generate token
+
+          var jwtOptions = new TokenProviderOptions
+          {
+            Audience = audience,
+            Issuer = issure,
+            SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256)
+          };
+
+          app.UseMiddleware<TokenProviderMiddleware>(Options.Create(jwtOptions));
+
+          //validation key
+
+
+          var tokenValidationParameters = new TokenValidationParameters
+          {
+            //The signing key must match !
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = signingKey,
+
+            //Validate the JWT Issuer (iss) claim
+            ValidateIssuer = true,
+            ValidIssuer = issure,
+
+            //validate the JWT Audience (aud) claim
+
+            ValidateAudience = true,
+            ValidAudience = audience,
+
+            //validate the token expiry
+            ValidateLifetime = true,
+
+            // If you  want to allow a certain amout of clock drift
+            ClockSkew = TimeSpan.Zero
+          };
+
+          app.UseJwtBearerAuthentication(new JwtBearerOptions
+          {
+            AutomaticAuthenticate = false,
+            AutomaticChallenge = true,
+            TokenValidationParameters = tokenValidationParameters,
+            AuthenticationScheme = "Bearer"
+          });
+
+          //end jwt token config
+          app.UseApplicationInsightsRequestTelemetry();
+
+
+          app.UseDefaultFiles();
+          app.UseStaticFiles();
+
+          app.UseMvcWithDefaultRoute();
+
+          //app.UseMvc(routes =>
+          //{
+          //    routes.MapRoute(
+          //        name: "default",
+          //        template: "{controller=Default}/{action=Index}/{id?}");
+          //});
+
+          app.UseMvc();
+
+          app.Use(async (context, next) =>
+          {
+            await next();
+            if (context.Response.StatusCode == 404 &&
+                !Path.HasExtension(context.Request.Path.Value) &&
+                !context.Request.Path.Value.StartsWith("/api/"))
             {
-                loggerFactory.AddSerilog();
-
-                Log.Logger().Information("Application is starting...");
-
-                Log.Logger().Information("Configuration:");
-
-                ConfigurationProvider.GetType().GetProperties().ToList().ForEach(prop =>
-                {
-                    Log.Logger().Information("[{name}] = '{value}'", prop.Name,
-                        prop.GetValue(ConfigurationProvider));
-                });
-
-                DateTimeContext.Initialize(ConfigurationProvider.TimeZone);
-
-                Log.Logger().Information("Configure EF Mappings...");
-
-                MappingConfig.RegisterMappings();
-                
-                //todo find out ????
-                if (env.IsDevelopment())
-                {
-                   // app.UseDeveloperExceptionPage();
-                   // app.UseBrowserLink();
-                }
-
-                app.UseDefaultFiles();
-                app.UseStaticFiles();
-
-                app.UseMvcWithDefaultRoute();
-
-                //app.UseMvc(routes =>
-                //{
-                //    routes.MapRoute(
-                //        name: "default",
-                //        template: "{controller=Default}/{action=Index}/{id?}");
-                //});
-
-              app.UseMvc();
-
-        app.Use(async (context, next) => 
-                {
-                    await next();
-                    if (context.Response.StatusCode == 404 &&
-                        !Path.HasExtension(context.Request.Path.Value) &&
-                        !context.Request.Path.Value.StartsWith("/api/"))
-                    {
-                        context.Request.Path = "/index.html";
-                        await next();
-                    }
-                });
-
+              context.Request.Path = "/index.html";
+              await next();
             }
-            catch (Exception e)
-            {
-                Log.Logger().Error(e, "Application failed to start");
-                throw;
-            }
-            finally
-            {
-                stopwatch.Stop();
+          });
+        }
+        catch (Exception e)
+        {
+          Log.Logger().Error(e, "Application failed to start");
+          throw;
+        }
+        finally
+        {
+          stopwatch.Stop();
 
-                Log.Logger().Information("Startup time: {Seconds}s", stopwatch.Elapsed.Seconds);
-            }
+          Log.Logger().Information("Startup time: {Seconds}s", stopwatch.Elapsed.Seconds);
+        }
         }
     }
 }
